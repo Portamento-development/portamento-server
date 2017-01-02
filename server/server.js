23fa091cdf6554917140df110dbb19cd7873881a
Andrew Beers <andybeers@gmail.com>
2017-01-02 13:30:09 -0800

    Set up basic server files

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./lib/app');
+const port = process.env.PORT || 3000;
+require('./lib/setup-mongoose');
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('Server started on port: ', server.address().port);
+});
